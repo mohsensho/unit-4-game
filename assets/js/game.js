568556c9e8523b8035b850ec74a508a0455535b9
Mohsen Shojaeifar <46658362+mohsensho@users.noreply.github.com>
2019-02-17 03:30:57 -0800

    Project completed
    this project added to my portfolio as well

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
-    let isYourCharacterSelected=0;
+    $( "#btnAttack" ).hide();
+    $( "#btnAttack" ).addClass("cursor-pointer");
     // start of tooltip js
     $(function () {
         $('[data-toggle="tooltip"]').tooltip();
@@ -14,33 +15,137 @@ $(document).ready(function(){
     // end of tooltip js
     // start of js for the characters selecting
     let charObject = {
+      isUserCharacterSelected : false,
+      isEnemyCharacterSelected : false,
+      userHealthPoint : -1,
+      enemyHealthPoint : -1,
+      userBasePower : -1,
+      userBasePowerExtend : -1,
+      enemyBasePower : -1,
+      userHurts : 0,
+      enemyHurts : 0,
       charactersNames : ['obi','luke','oarth','darth'],
-      defaultPower : [120,100,150,180]
-
+      healthPoint : [120,100,150,180],
+      basePower : [2,3,4,3],
+      userCharacterId : '',
+      enemyCharacterId : '',
+      
     }
     $(".characters").on("click", function(){
-      let characterId=$(this).attr("id");
-      jQuery.each(charObject.charactersNames, $.proxy(function(index, item) {
+      if(!charObject.isUserCharacterSelected){
+          charObject.userCharacterId=$(this).attr("id");
+          jQuery.each(charObject.charactersNames, $.proxy(function(index, item) {
 
-        if(characterId == item && !isYourCharacterSelected){
-            $(this).tooltip('hide');
-            $("#"+item).remove();
-            $('#yourField').html(`
-                <div id="obi">
-                    <div class="row">
-                        <img class="imgCharacter yourCharacter img-fluid rounded-circle float-left" alt="Obi Wan Kenobi" src="assets/images/${item}.jpg"/>
-                    </div>
-                    <div class="row">
-                        <div id="fightingScore" class="tooltip-inner">Power ${charObject.defaultPower[index]}</div>
-                    </div>
-                </div>`);
-            isYourCharacterSelected=1;
-            $('.characters').addClass("enemyCharacter");
-            $('.characters').addClass('cursor-crosshair');
-            $('#userMessage').text('Select your enemy!');
-          }
+            if(charObject.userCharacterId == item && !charObject.isUserCharacterSelected){
+                $(this).tooltip('hide');
+                $("#"+item).remove();
+                $('#yourField').html(`
+                    <div id="${charObject.userCharacterId}">
+                        <div class="row">
+                            <img class="imgCharacter yourCharacter img-fluid rounded-circle float-left" src="assets/images/${item}.jpg"/>
+                        </div>
+                        <div class="row">
+                            <div id="userFightingScore" class="tooltip-inner">Power ${charObject.healthPoint[index]}</div>
+                        </div>
+                    </div>`);
+                charObject.isUserCharacterSelected = true;
+                // saving and removing selected character by user from array
+                charObject.userHealthPoint =  charObject.healthPoint[index];
+                charObject.userBasePower =  charObject.basePower[index];
+                charObject.userBasePowerExtend =  charObject.basePower[index];
+                charObject.charactersNames.splice(index,1);
+                charObject.healthPoint.splice(index,1);
+                charObject.basePower.splice(index,1);
+            
+                $('.characters').addClass("enemyCharacter");
+                $('.characters').addClass('cursor-crosshair');
+                $('#userMessage').text('Select your enemy!');
+              }
+    
+        }, this));
+      }
+      else{
+          charObject.enemyCharacterId=$(this).attr("id");
+          jQuery.each(charObject.charactersNames, $.proxy(function(index, item) {
+          
+            if(charObject.enemyCharacterId == item && !charObject.isEnemyCharacterSelected){
+                $(this).tooltip('hide');
+                $("#"+item).remove();
+                $("#enemyField").html(`
+                    <div id="${charObject.enemyCharacterId}">
+                        <div class="row">
+                            <img class="imgCharacter enemyCharacter img-fluid rounded-circle float-left" src="assets/images/${item}.jpg"/>
+                        </div>
+                        <div class="row">
+                            <div id="enemyFightingScore" class="tooltip-inner">Power ${charObject.healthPoint[index]}</div>
+                        </div>
+                    </div>`);
+                charObject.isEnemyCharacterSelected = true;
+                // saving and removing selected character by enemy from array
+                charObject.enemyHealthPoint =  charObject.healthPoint[index];
+                charObject.enemyBasePower =  charObject.basePower[index];
+                charObject.charactersNames.splice(index,1);
+                charObject.healthPoint.splice(index,1);
+                charObject.basePower.splice(index,1);
+                
+                $('.characters').addClass("enemyCharacter");
+                $('.characters').addClass('cursor-not-allowed');
+                $('#userMessage').text('Attack enabled!');
+                $( "#btnAttack" ).show();
+                $('#btnAttack').text('Attack');
+              }
+    
+            
+        }, this));
+      }
+      
+      
+    });
+    // enemy
+    //Attack Process:
+    $("#btnAttack").on("click", function(){
+        $('#userMessage').text('');
+         if(charObject.userHealthPoint>0)
+         {
+             if(charObject.charactersNames.length>0)
+             {
+                // user attack
+                charObject.enemyHealthPoint = charObject.enemyHealthPoint - (charObject.userBasePower * (Math.floor(Math.random() * 10) + 1));
+                $('#enemyFightingScore').html(`Power ${charObject.enemyHealthPoint}`);
+                if (charObject.enemyHealthPoint<=0){
+                    $('#enemyFightingScore').html(`Power --`);
+                    $('#userMessage').html("You Won this enemy! Select the next enemy.");
+                    charObject.isEnemyCharacterSelected = false;
+                    $('.characters').removeClass('cursor-not-allowed');
+                    $('.characters').removeClass('cursor-crosshair');
+                    $('.characters').addClass('cursor-pointer');
+                    $( "#btnAttack" ).hide();                 
+                }
+                charObject.userBasePower = charObject.userBasePower + charObject.userBasePowerExtend;
+                // enemy attack
+                charObject.userHealthPoint -= charObject.enemyBasePower * (Math.floor(Math.random() * 10) + 1);
+                $('#userFightingScore').html(`Power ${charObject.userHealthPoint}`);
+                if (charObject.userHealthPoint<=0){
+                    $('#userFightingScore').html(`Power --`);
+                    alert("You Lost!");
+                    $('#userMessage').html('<a href="#" onclick="location.reload();">Play again!? Reload the game!</a>');
+                    $( "#btnAttack" ).hide();
+                }
+            }
+            else
+            {
+                alert("You Won!");
+                    $('#userMessage').html('<a href="#" onclick="location.reload();">Play again!? Reload the game!</a>');
+                    $( "#btnAttack" ).hide();
+            }
+         }
+         else{
+             alert("You Lost! Game Over!");
+             $('#userMessage').html('<a href="#" onclick="location.reload();">Play again!? Reload the game!</a>');
+             $( "#btnAttack" ).hide();
+         }
         
-    }, this));
     });
 
-  });
+
+  }); // document ready
